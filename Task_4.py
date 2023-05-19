3cf03c2eb1a98a79047f305ce84d8a417263a4ac
Julia Morozova <juliamorozova@fox-2.local>
2023-05-19 11:14:08 +0200

    add homework 1

---
+++ Task_4.py
@@ -0,0 +1,8 @@
+def check_number(num):
+    if num % 2 == 0:
+        return 'Четное'
+    else:
+        return 'Не четное'
+
+
+print(check_number(3))
---
+++ Task_7.py
@@ -0,0 +1,9 @@
+from Task_6 import sum_num
+
+
+def get_sum(a, b):
+    result = sum_num(a, b)
+    return result
+
+
+print(get_sum(3, 5))
